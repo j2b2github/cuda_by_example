837fca33133c34fa980d58b0dbd00abe406496d9
radiomanias <radiomanias.gmail.com>
2022-10-30 21:12:47 +0900

    Add project files.

---
+++ CUDA_by_Example/gpuErrchk.cuh
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+// 에러 핸들러 출처: https://stackoverflow.com/questions/14038589/what-is-the-canonical-way-to-check-for-errors-using-the-cuda-runtime-api
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char* file, int line, bool abort = true)
+{
+	if (code != cudaSuccess)
+	{
+		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+		if (abort) exit(code);
+	}
+}
